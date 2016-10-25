a425255deb0c7e26163f5b5753161640aa8266cf
Espen Hagen <ehagen@nmbu.no>
2016-10-25 12:08:28 +0200

    added a COMM.Barrier() before trying to merge gdf files, some cleanup

--- examples/Hagen_et_al_2016_cercor/nest_output_processing.py
+++ examples/Hagen_et_al_2016_cercor/nest_output_processing.py
@@ -24,10 +24,12 @@ flattenlist = lambda lst: sum(sum(lst, []),[])
 
 def get_raw_gids(model_params):
     '''
-    Reads text file containing gids of neuron populations as created within the NEST simulation. 
-    These gids are not continuous as in the simulation devices get created in between.
+    Reads text file containing gids of neuron populations as created within the
+    NEST simulation. These gids are not continuous as in the simulation devices
+    get created in between.
     '''
-    gidfile = open(os.path.join(model_params.raw_nest_output_path, model_params.GID_filename),'r') 
+    gidfile = open(os.path.join(model_params.raw_nest_output_path,
+                                model_params.GID_filename), 'r') 
     gids = [] 
     for l in gidfile :
         a = l.split()
@@ -43,14 +45,20 @@ def merge_gdf(model_params, raw_label='spikes_', file_type='gdf',
     '''
     #some preprocessing
     raw_gids = get_raw_gids(model_params)
-    pop_sizes = [raw_gids[i][1]-raw_gids[i][0]+1 for i in np.arange(model_params.Npops)]
+    pop_sizes = [raw_gids[i][1]-raw_gids[i][0]+1
+                 for i in np.arange(model_params.Npops)]
     raw_first_gids =  [raw_gids[i][0] for i in np.arange(model_params.Npops)]
-    converted_first_gids = [int(1 + np.sum(pop_sizes[:i])) for i in np.arange(model_params.Npops)]
+    converted_first_gids = [int(1 + np.sum(pop_sizes[:i]))
+                            for i in np.arange(model_params.Npops)]
+
+    # The network simulation may not have completely finished across RANKS
+    COMM.Barrier()
 
     for pop_idx in np.arange(model_params.Npops):
         if pop_idx % SIZE == RANK:
             files = glob(os.path.join(model_params.raw_nest_output_path,
-                                      raw_label + str(pop_idx) + '*.' + file_type))
+                                      raw_label + '{}*.{}'.format(pop_idx,
+                                                                  file_type)))
             gdf = [] # init
             for f in files:
                 new_gdf = helpers.read_gdf(f)
@@ -58,10 +66,10 @@ def merge_gdf(model_params, raw_label='spikes_', file_type='gdf',
                     line[0] = line[0] - raw_first_gids[pop_idx] + converted_first_gids[pop_idx]
                     gdf.append(line)
             
-            print 'writing: %s' % os.path.join(model_params.spike_output_path,
-                                               fileprefix + '_%s.gdf' % model_params.X[pop_idx])
+            print 'writing: {}'.format(os.path.join(model_params.spike_output_path,
+                                                    fileprefix + '_{}.gdf'.format(model_params.X[pop_idx])))
             helpers.write_gdf(gdf, os.path.join(model_params.spike_output_path,
-                                                fileprefix + '_%s.gdf' % model_params.X[pop_idx]))
+                                                fileprefix + '_{}.gdf'.format(model_params.X[pop_idx])))
     
     COMM.Barrier()
 
