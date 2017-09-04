e1543238b9cf448efe8eeb6facb5217b365a479f
Espen Hagen <e.hagen@fz-juelich.de>
2017-09-04 09:08:31 +0200

    fix networkSimParams dict

--- examples/Hagen_et_al_2016_cercor/cellsim16popsParams_spikegen.py
+++ examples/Hagen_et_al_2016_cercor/cellsim16popsParams_spikegen.py
@@ -782,6 +782,7 @@ class multicompartment_params(point_neuron_network_params):
             'label' :       'population_spikes',
             'ext' :         'gdf',
             'GIDs' : self.get_GIDs(),
+            'X' : self.X,
         }
 
 
