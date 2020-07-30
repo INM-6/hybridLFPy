12de12c5eb5a70007e1452a93259069a297a3d74
Espen Hagen <espehage@fys.uio.no>
2020-07-30 10:21:48 +0200

    record voltages and inputs from only 20 neurons due to HD use

--- examples/Hagen_et_al_2016_cercor/cellsim16popsParams_default.py
+++ examples/Hagen_et_al_2016_cercor/cellsim16popsParams_default.py
@@ -486,7 +486,7 @@ class point_neuron_network_params(general_params):
         if self.record_fraction_neurons_voltage:
             self.frac_rec_voltage = 0.1
         else:
-            self.n_rec_voltage = 100
+            self.n_rec_voltage = 20 #100
 
         # whether to record weighted input spikes from a fixed fraction of neurons in each population
         self.record_fraction_neurons_input_spikes = False
@@ -494,7 +494,7 @@ class point_neuron_network_params(general_params):
         if self.record_fraction_neurons_input_spikes:
             self.frac_rec_input_spikes = 0.1
         else:
-            self.n_rec_input_spikes = 100
+            self.n_rec_input_spikes = 20 #100
 
         # number of recorded neurons for depth resolved input currents
         self.n_rec_depth_resolved_input = 0
