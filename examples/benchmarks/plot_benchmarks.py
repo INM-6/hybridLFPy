ad70dbd76cbb575be51488d09631f5b88cb30a4a
Espen Hagen <2492641+espenhgn@users.noreply.github.com>
2021-08-19 13:02:45 +0200

    plot updates

--- examples/benchmarks/plot_benchmarks.py
+++ examples/benchmarks/plot_benchmarks.py
@@ -13,13 +13,13 @@ import brunel_alpha_nest as BN
 PS0 = ParameterSpace('PS0.txt')
 keys = PS0.range_keys()
 
-fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
 for h in ['HOST', 'HOSTNAME']:
     if h in os.environ:
         host = os.environ[h]
         break
 
-ax.set_title(f"network size: {5 * BN.order}; host: {host}")
+# ax.set_title(f"Network size: {5 * BN.order}")
 
 for i, SIM_SCRIPT in enumerate(PS0['SIM_SCRIPT']):
     times_pop = []
@@ -39,16 +39,17 @@ for i, SIM_SCRIPT in enumerate(PS0['SIM_SCRIPT']):
         with open(os.path.join(md5, 'time_run.txt'), 'r') as f:
             times_run.append(float(f.readline()))
 
-    label = f"{'LFPy' if SIM_SCRIPT == 'example_brunel.py' else 'Arbor'} pop"
+    # label = f"{'hybridLFPy' if SIM_SCRIPT == 'example_brunel.py' else 'Arbor'}"
+    label='total sim. time'
     ax.loglog(NTASKS, times_pop, '-o', label=label, base=2)
 
-    label = f"{'LFPy' if SIM_SCRIPT == 'example_brunel.py' else 'Arbor'} run"
-    ax.loglog(NTASKS, times_run, ':o', label=label, base=2)
+    # label = f"{'hybridLFPy' if SIM_SCRIPT == 'example_brunel.py' else 'Arbor'} run"
+    # ax.loglog(NTASKS, times_run, ':o', label=label, base=2)
 
 ax.legend()
-ax.set_xlabel('NTASKS')
-ax.set_ylabel('time (ms)')
+ax.set_xlabel('# MPI procs.', labelpad=0)
+ax.set_ylabel('time (s)', labelpad=0)
 
-fig.savefig('PS0.pdf')
+fig.savefig('PS0.pdf', bbox_inches='tight')
 
 plt.show()
